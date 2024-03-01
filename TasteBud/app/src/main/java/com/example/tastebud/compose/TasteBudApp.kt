8415677a05323639d01ff7eacf8cc94610e07a2b
juliengo141 <juliengo141@gmail.com>
2024-03-01 11:54:26 -0500

    Created the navigation functionality between the homescreen and the recipe details screen

--- TasteBud/app/src/main/java/com/example/tastebud/compose/TasteBudApp.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/compose/TasteBudApp.kt
@@ -1,106 +1,22 @@
 package com.example.tastebud.compose
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.dp
-
-//import android.app.Activity
-//import android.content.Intent
-//import android.net.Uri
-//import androidx.compose.foundation.layout.Arrangement
-//import androidx.compose.foundation.layout.Column
-//import androidx.compose.foundation.layout.fillMaxWidth
-//import androidx.compose.foundation.layout.padding
-//import androidx.compose.material.icons.Icons
-//import androidx.compose.material.icons.filled.Add
-//import androidx.compose.material3.*
-//import androidx.compose.runtime.*
-//import androidx.compose.ui.Modifier
-//import androidx.compose.ui.graphics.Color
-//import androidx.compose.ui.platform.LocalContext
-//import androidx.compose.ui.text.style.TextAlign
-//import androidx.compose.ui.unit.dp
-//import androidx.navigation.NavHostController
-//import androidx.navigation.compose.NavHost
-//import androidx.navigation.compose.composable
-//import androidx.navigation.compose.rememberNavController
-//import com.example.tastebud.compose.home.HomeScreen
-//import com.example.tastebud.compose.recipeDetail.RecipeDetailsScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.tastebud.compose.home.HomeScreen
+import com.example.tastebud.compose.recipeDetail.RecipeDetailScreen
 
 @Composable
 fun TasteBudApp() {
-//    val navController = rememberNavController()
-//    TasteBudNavHost(
-//        navController = navController
-//    )
-    ScaffoldExample()
-
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun ScaffoldExample() {
-    var presses by remember { mutableIntStateOf(0) }
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text("TasteBud")
-                }
-            )
-        },
-        bottomBar = {
-            BottomAppBar(
-                containerColor = MaterialTheme.colorScheme.primaryContainer,
-                contentColor = MaterialTheme.colorScheme.primary,
-            ) {
-                var text = remember { mutableStateOf("Home")}
-                Button(onClick = { text.value = "TODO: got to home screen"}) {
-                    Text(
-                        modifier = Modifier
-                            .fillMaxWidth(),
-                        textAlign = TextAlign.Center,
-                        text = text.value,
-                    )
-                }
-            }
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = { presses++ }) {
-                Icon(Icons.Default.Add, contentDescription = "Add")
-            }
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "homeScreen") {
+        composable("homeScreen") {
+            HomeScreen(navController = navController)
         }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding),
-            verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text =
-                """
-                    You have pressed the floating action button $presses times.
-                """.trimIndent(),
-            )
-            var text = remember { mutableStateOf("Recipe")}
-            Button(onClick = { text.value = "TODO: Go to recipeDetail screen"}) {
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    textAlign = TextAlign.Center,
-                    text = text.value,
-                )
-            }
+        composable("recipeDetailsScreen") {
+            RecipeDetailScreen(navController = navController)
         }
     }
 }
+
--- TasteBud/app/src/main/java/com/example/tastebud/compose/home/HomeScreen.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/compose/home/HomeScreen.kt
@@ -1,5 +1,84 @@
 package com.example.tastebud.compose.home
 
-class HomeScreen(onRecipeClick: () -> Unit) {
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 
+@Composable
+fun HomeScreen(navController: NavController) {
+    ScaffoldHome(navController)
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScaffoldHome(navController: NavController) {
+    var presses by remember { mutableIntStateOf(0) }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("TasteBud")
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary,
+            ) {
+                var text = remember { mutableStateOf("Home") }
+                Button(onClick = {
+                    navController.navigate("homeScreen")
+                }) {
+                    Text(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        text = text.value,
+                    )
+                }
+            }
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = { presses++ }) {
+                Icon(Icons.Default.Add, contentDescription = "Add")
+            }
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            Text(
+                modifier = Modifier.padding(8.dp),
+                text =
+                """
+                    This is the HOMESCREEN
+                    You have pressed the floating action button $presses times.
+                """.trimIndent(),
+            )
+            var text = remember { mutableStateOf("Recipe") }
+            Button(onClick = {
+                navController.navigate("recipeDetailsScreen")
+            }) {
+                Text(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center,
+                    text = text.value,
+                )
+            }
+        }
+    }
 }
