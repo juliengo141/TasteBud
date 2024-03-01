ecf530baadca708956e10866473b186d08921fdc
Julie Ngo <julie.ngo1@uwaterloo.ca>
2024-03-01 12:20:27 -0500

    created the scaffold on the main screen and buttons to redirect to home and recipe

--- TasteBud/app/src/main/java/com/example/tastebud/MainActivity.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.tastebud
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.tastebud.compose.TasteBudApp
 import com.example.tastebud.ui.theme.TasteBudTheme
 import com.mongodb.kotlin.client.coroutine.MongoDatabase
 import com.mongodb.kotlin.client.coroutine.MongoClient
@@ -27,35 +34,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             TasteBudTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                TasteBudApp()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-
-
-@Composable
-fun GreetingPreview() {
-    TasteBudTheme {
-        Greeting("Android")
-    }
-}
 fun getDatabase(): MongoDatabase{
     val client = MongoClient.create(connectionString = "mongodb+srv://tastebuddev102:<TasteBud102>@cluster0.n4b5xe3.mongodb.net/?retryWrites=true&w=majority")
     return client.getDatabase(databaseName = "sample_restaurants")
---
+++ TasteBud/app/src/main/java/com/example/tastebud/data/Recipe.kt
@@ -0,0 +1,12 @@
+package com.example.tastebud.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "recipes")
+data class Recipe(
+    @PrimaryKey @ColumnInfo(name = "id") val recipeId: String,
+    val name: String,
+    val imageUrl: String = ""
+) { }
