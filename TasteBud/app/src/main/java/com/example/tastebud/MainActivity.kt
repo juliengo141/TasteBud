569886bee3f9c37e2b0f97c2124bbf2afc0f3d18
Lavan Nithi <lavannithi2@gmail.com>
2024-03-01 21:36:06 -0500

    Connect to MongoDB

--- TasteBud/app/src/main/java/com/example/tastebud/MainActivity.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/MainActivity.kt
@@ -3,27 +3,21 @@ package com.example.tastebud
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import com.example.tastebud.MongoDBExample.insertDocument
 import com.example.tastebud.compose.TasteBudApp
 import com.example.tastebud.ui.theme.TasteBudTheme
-import com.mongodb.kotlin.client.coroutine.MongoDatabase
-import com.mongodb.kotlin.client.coroutine.MongoClient
-import kotlinx.coroutines.runBlocking
+import com.mongodb.ConnectionString
+import com.mongodb.MongoClientSettings
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import org.bson.Document
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         //val database = getDatabase()
 
 //        runBlocking{
@@ -32,15 +26,41 @@ class MainActivity : ComponentActivity() {
 //            }
 //        }
         super.onCreate(savedInstanceState)
+
         setContent {
             TasteBudTheme {
                 TasteBudApp()
             }
         }
+        insertDocument()
     }
 }
 
-fun getDatabase(): MongoDatabase{
-    val client = MongoClient.create(connectionString = "mongodb+srv://tastebuddev102:<TasteBud102>@cluster0.n4b5xe3.mongodb.net/?retryWrites=true&w=majority")
-    return client.getDatabase(databaseName = "sample_restaurants")
+object MongoDBConnection {
+    val uri = "mongodb+srv://tastebuddev102:<TasteBud102>@cluster0.n4b5xe3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+    fun getMongoClient(): MongoClient {
+        val connectionString = ConnectionString(uri)
+        val settings = MongoClientSettings.builder()
+            .applyConnectionString(connectionString)
+            .build()
+        return MongoClients.create(settings)
+    }
+
+}
+
+object MongoDBExample {
+    fun insertDocument() {
+        val mongoClient = MongoDBConnection.getMongoClient()
+        val database: MongoDatabase = mongoClient.getDatabase("RecipeTable")
+        val collection: MongoCollection<Document> = database.getCollection("RecipeData")
+
+        val document = Document("name", "John Doe")
+            .append("email", "johndoe@example.com")
+            .append("age", 30)
+
+        collection.insertOne(document)
+
+        mongoClient.close()
+    }
 }
