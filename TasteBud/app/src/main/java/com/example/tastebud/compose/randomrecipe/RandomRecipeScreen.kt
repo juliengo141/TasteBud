df49b8c647acb4826b6ac4be00e1f09202d596c0
Lavan Nithianandarajah <lnithianandarajah@uwaterloo.ca>
2024-03-11 12:38:03 -0400

    Resolve "Layout of Screen"

--- TasteBud/app/src/main/java/com/example/tastebud/compose/randomrecipe/RandomRecipeScreen.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/compose/randomrecipe/RandomRecipeScreen.kt
@@ -4,9 +4,13 @@ import NavBarScaffold
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 
 
@@ -18,16 +22,30 @@ fun RandomRecipeScreen(navController: NavController) {
 @Composable
 fun RandomRecipeContent(navController: NavController, innerPadding: PaddingValues) {
     Column(
-        modifier = Modifier.padding(innerPadding),
-        verticalArrangement = Arrangement.spacedBy(16.dp),
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
     ) {
         Text(
-            modifier = Modifier.padding(8.dp),
+            modifier = Modifier.padding(8.dp)
+                .fillMaxWidth(),
+
             text = """
-                    This is the Recipe Roulette
-                """.trimIndent(),
+                    Not sure what to make? Let us decide for you!
+                """.trimIndent(), fontWeight = FontWeight.Bold, color = Color.Green, fontSize = 24.sp,
+            textAlign = TextAlign.Center,
         )
-
-
+        Button(
+            onClick = {
+                //Call flashcards Tinder Swipe
+            },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.Black
+            ),
+            modifier = Modifier.padding(24.dp).height(52.dp)
+        ) {
+            Text("Generate Recipe...", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+        }
     }
 }
