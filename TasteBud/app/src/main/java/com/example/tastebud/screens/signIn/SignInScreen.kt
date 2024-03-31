a27bae139492947486e194297799ae2744556d7a
Lavan Nithianandarajah <lnithianandarajah@uwaterloo.ca>
2024-03-31 14:08:48 -0400

    Styling for Login and Sign-in page

--- TasteBud/app/src/main/java/com/example/tastebud/screens/signIn/SignInScreen.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/screens/signIn/SignInScreen.kt
@@ -2,20 +2,35 @@ package com.example.tastebud.screens.home
 
 import NavBarScaffold
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.outlined.Security
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.tastebud.R
 import com.example.tastebud.components.HeadingTextComponent
 import com.example.tastebud.screens.SharedViewModel
+import com.example.tastebud.ui.theme.Inter
+import com.example.tastebud.ui.theme.TasteBudBackground
+import com.example.tastebud.ui.theme.TasteBudDarkGreen
+import com.example.tastebud.ui.theme.TasteBudGreen
 import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 
@@ -29,12 +44,26 @@ fun SignInScreen(navController: NavController, sharedViewModel: SharedViewModel)
 fun SignInContent(navController: NavController, sharedViewModel: SharedViewModel) {
     val auth = Firebase.auth
     Column(
+        modifier = Modifier.background(TasteBudBackground).fillMaxHeight(),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
+        Image(
+            painterResource(R.drawable.tastebud),
+            contentDescription = "",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
         val emailValue = remember { mutableStateOf(TextFieldValue()) }
         val passwordValue = remember { mutableStateOf(TextFieldValue()) }
 
-        HeadingTextComponent("Welcome Back")
+        Text(
+            text = "Welcome Back",
+            style = MaterialTheme.typography.headlineLarge,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            fontWeight = FontWeight.Bold,
+            color = Color.Black,
+            fontFamily = Inter
+        )
 
         OutlinedTextField(
             label = {
@@ -43,28 +72,62 @@ fun SignInContent(navController: NavController, sharedViewModel: SharedViewModel
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
             value = emailValue.value,
             singleLine = true,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
             onValueChange = {
                 emailValue.value = it
-            }
-        )
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Email,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(30.dp)
+                )
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                textColor = Color.Black,
+                cursorColor = TasteBudDarkGreen,
+                focusedBorderColor = TasteBudDarkGreen,
+                unfocusedBorderColor = Color.Gray,
+                focusedLabelColor = TasteBudDarkGreen
+            ),
+
+            )
         OutlinedTextField(
             label = {
                 Text("Password")
             },
+            textStyle = MaterialTheme.typography.bodyMedium,
             visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
             value = passwordValue.value,
             singleLine = true,
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
             onValueChange = {
                 passwordValue.value = it
-            }
+            },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Outlined.Security,
+                    contentDescription = "Localized description",
+                    modifier = Modifier.size(30.dp)
+                )
+            },
+            colors = TextFieldDefaults.outlinedTextFieldColors(
+                textColor = Color.Black,
+                cursorColor = TasteBudDarkGreen,
+                focusedBorderColor = TasteBudDarkGreen,
+                unfocusedBorderColor = Color.Gray,
+                focusedLabelColor = TasteBudDarkGreen
+            ),
         )
         Spacer(modifier = Modifier.padding(8.dp))
         Button(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth().padding(horizontal = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = TasteBudGreen,
+                contentColor = Color.White
+            ),
             onClick = {
                 auth.signInWithEmailAndPassword(
                     emailValue.value.text.trim(),
@@ -87,7 +150,14 @@ fun SignInContent(navController: NavController, sharedViewModel: SharedViewModel
             Text(text = "Login")
         }
         ClickableText(
-            text = AnnotatedString("Don't have an account yet? Register"),
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            text = AnnotatedString("Don't have an account yet? ") + AnnotatedString(
+                text = "Register",
+                spanStyle = androidx.compose.ui.text.SpanStyle(
+                    color = TasteBudGreen, // Set the color to green
+                    fontWeight = FontWeight.Bold,
+                ),
+            ),
             onClick = {
                 navController.navigate("signUpScreen")
             }
