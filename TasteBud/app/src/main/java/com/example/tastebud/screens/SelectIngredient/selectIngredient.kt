5b93b4caa268da951fd7cbaab250d22221de8029
Lavan Nithianandarajah <lnithianandarajah@uwaterloo.ca>
2024-04-01 22:44:26 -0400

    Resolve "Final Select Ingredients Screen"

--- TasteBud/app/src/main/java/com/example/tastebud/screens/SelectIngredient/selectIngredient.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/screens/SelectIngredient/selectIngredient.kt
@@ -2,6 +2,7 @@ package com.example.tastebud.screens.home
 
 import NavBarScaffold
 import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Done
@@ -17,12 +18,16 @@ import androidx.compose.ui.unit.em
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.tastebud.screens.SharedViewModel
+import com.example.tastebud.ui.theme.Inter
+import com.example.tastebud.ui.theme.TasteBudAccent
+import com.example.tastebud.ui.theme.TasteBudBackground
+import com.example.tastebud.ui.theme.TasteBudGreen
 
-var fridgeIngredients = listOf<String>("Eggs", "Chicken", "Paneer", "Milk", "Onions", "Rice", "Noodles", "Tomato", "Potatoes", "Tuna", "Spinach", "Pasta", "Beef", "Pork Chops", "Honey" )
+var fridgeIngredients = listOf<String>("Eggs", "Chicken", "Paneer", "Milk", "Onions", "Rice", "Noodles", "Tomato", "Potatoes", "Tuna", "Spinach", "Pasta", "Beef", "Honey" )
 @Composable
 fun SelectIngredientScreen(navController: NavController, sharedViewModel: SharedViewModel) {
     val selectedIngredients = remember { mutableStateListOf<String>() }
-    NavBarScaffold(navController, "What's in the fridge") { selectContent(navController, it, selectedIngredients) }
+    NavBarScaffold(navController, "What's in the Fridge?") { selectContent(navController, it, selectedIngredients) }
 }
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -31,7 +36,8 @@ fun selectContent(navController: NavController, innerPadding: PaddingValues, sel
     Column(
         modifier = Modifier
             .padding(innerPadding)
-            .padding(20.dp, 16.dp),
+            .padding(20.dp, 16.dp)
+            .background(TasteBudBackground),
         verticalArrangement = Arrangement.spacedBy(16.dp),
     ) {
         Text(
@@ -40,6 +46,7 @@ fun selectContent(navController: NavController, innerPadding: PaddingValues, sel
                     Choose your Ingredients
                 """.trimIndent(),
             fontWeight = FontWeight.Black,
+            fontFamily = Inter,
             fontSize = 48.sp,
             lineHeight = 1.3.em,
         )
@@ -48,11 +55,12 @@ fun selectContent(navController: NavController, innerPadding: PaddingValues, sel
             text = """
                     Choose which ingredients from your fridge you want to use to make your recipe with!
                 """.trimIndent(),
+            fontFamily = Inter,
         )
 
         FlowRow(
             modifier = Modifier
-                .padding(16.dp, 0.dp),
+                .padding(8.dp, 0.dp),
             horizontalArrangement = Arrangement.spacedBy(8.dp)
         ){
             for(ingredient in fridgeIngredients){
@@ -74,13 +82,13 @@ fun selectContent(navController: NavController, innerPadding: PaddingValues, sel
                 Log.d("SelectedIngredients", selectedIngredients.toString())
             },
             colors = ButtonDefaults.buttonColors(
-                contentColor = Color.Black
+                containerColor = TasteBudGreen
             ),
             modifier = Modifier
                 .padding(16.dp)
                 .height(40.dp)
         ) {
-            Text(text = "Continue", fontWeight = FontWeight.Bold, fontSize = 20.sp, color = Color.White, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center)
+            Text(text = "Continue", fontWeight = FontWeight.Bold, fontSize = 20.sp, color = Color.White, modifier = Modifier.fillMaxWidth(), textAlign = TextAlign.Center, fontFamily = Inter,)
         }
     }
 }
@@ -96,9 +104,13 @@ fun FilterChipExample(title: String, onIngredientSelected: (Boolean) -> Unit) {
             onIngredientSelected(selected)
         },
         label = {
-            Text(text = title, modifier = Modifier.padding(vertical = 8.dp, horizontal = 12.dp))
+            Text(text = title, modifier = Modifier.padding(vertical = 8.dp, horizontal = 10.dp))
         },
         selected = selected,
+        colors = FilterChipDefaults.filterChipColors(
+            selectedContainerColor = TasteBudAccent
+        ),
+
         leadingIcon = if (selected) {
             {
                 Icon(
