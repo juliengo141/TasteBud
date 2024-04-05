17db2b36aed3234cc8c6d3c859cebb88f2977c72
juliengo141 <juliengo141@gmail.com>
2024-04-04 22:33:28 -0400

    Made the stars increase in difficulty in recipe road

--- TasteBud/app/src/main/java/com/example/tastebud/screens/reciperoad/RecipeRoadScreen.kt
+++ TasteBud/app/src/main/java/com/example/tastebud/screens/reciperoad/RecipeRoadScreen.kt
@@ -6,8 +6,6 @@ import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -23,8 +21,8 @@ import com.example.tastebud.data.Ingredient
 import com.example.tastebud.data.Instruction
 import com.example.tastebud.data.Recipe
 import com.example.tastebud.screens.SharedViewModel
-import com.example.tastebud.ui.theme.TasteBudGreen
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.firestore
 
 @Composable
@@ -63,7 +61,7 @@ fun RecipeCard(recipe: Recipe, navController: NavController, sharedViewModel: Sh
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp)
-            .height(220.dp),
+            .height(250.dp),
         elevation = CardDefaults.cardElevation(10.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White),
         onClick = {
@@ -100,7 +98,7 @@ fun RecipeCard(recipe: Recipe, navController: NavController, sharedViewModel: Sh
                 )
                 Spacer(modifier = Modifier.height(8.dp))
                 Text(
-                    text = "Difficulty:",
+                    text = "Difficulty: ",
                     fontSize = 16.sp,
                     fontWeight = FontWeight.Medium,
                     modifier = Modifier.padding(horizontal = 16.dp)
@@ -122,89 +120,118 @@ fun CuisineRecipes(sharedViewModel: SharedViewModel){
     val recipesRef = db.collection("Recipes")
     recipesRef.whereArrayContains("cuisines", sharedViewModel.cuisine)
         .orderBy("difficulty")
-        .limit(5)
         .get()
         .addOnSuccessListener { result ->
+            val filteredRecipes = mutableListOf<DocumentSnapshot>()
+            var countOneStar = 0
+            var countTwoStar = 0
+            var countThreeStar = 0
+            var countFourStar = 0
+            var countFiveStar = 0
             for (document in result) {
-                if (document.exists()) {
-                    val ingredients = document.data?.get("extendedIngredients") as List<Map<String, Any>>
-                    //For ingredients mapping
-                    for (ingredientData in ingredients) {
-                        var name = ingredientData["name"] as? String
-                        val quantity = ingredientData["amount"].toString()
-                        var unit = ingredientData["unit"] as? String
-                        val id = ingredientData["id"].toString()
-                        var og = ingredientData["original"] as? String
-                        var image = ingredientData["image"] as? String
-                        if(name == null){
-                            name =""
-                        }
-                        if(unit == null){
-                            unit =""
-                        }
-                        if(og == null){
-                            og =""
-                        }
-                        if(image == null){
-                            image =""
-                        }
-                        val ingredient = Ingredient(id, name, og, image, quantity, unit)
-                        testIngredientList.add(ingredient)
-                    }
-                    val steps = mutableListOf<Instruction>()
-
-                    val instructions = document.data?.get("analyzedInstructions") as List<Map<String, Any>>
-                    for(instructionData in instructions) {
-                        val stepNum = instructionData["number"] as Long
-                        val step = instructionData["step"].toString()
-
-                        val equipmentList = mutableListOf<Equipment>()
-                        //val equipment = instructionData.data?.get("equipment") as List<Map<String, Any>>
-                        val equipment = instructionData["equipment"] as List<Map<String, Any>>
-                        for(equipmentData in equipment) {
-                            val id = equipmentData["id"].toString()
-                            val name = equipmentData["name"].toString()
-                            val image = equipmentData["image"].toString()
-                            val e = Equipment(id, name, image)
-                            equipmentList.add(e)
-                        }
-
-                        val instructionIngredientsList = mutableListOf<Equipment>()
-                        val i = instructionData["ingredients"] as List<Map<String, Any>>
-                        for(instructionIngredientData in i) {
-                            val id = instructionIngredientData["id"].toString()
-                            val name = instructionIngredientData["name"].toString()
-                            val image = instructionIngredientData["image"].toString()
-                            val e = Equipment(id, name, image)
-                            instructionIngredientsList.add(e)
-                        }
-
-                        val instruction = Instruction(stepNum, step, instructionIngredientsList, equipmentList)
-                        steps.add(instruction)
-
-                    }
-
-                    var recipe = Recipe(
-                        (document.data?.get("id")).toString(),
-                        (document.data?.get("title")).toString(),
-                        (document.data?.get("image")).toString(),
-                        (document.data?.get("readyInMinutes")).toString() + " mins",
-                        (document.data?.get("servings")) as Long,
-                        (document.data?.get("cuisines")) as List<String>,
-                        (document.data?.get("vegetarian")) as Boolean,
-                        (document.data?.get("vegan")) as Boolean,
-                        (document.data?.get("glutenFree")) as Boolean,
-                        (document.data?.get("dairyFree")) as Boolean,
-                        (document.data?.get("veryHealthy")) as Boolean,
-                        (document.data?.get("cheap")) as Boolean,
-                        (document.data?.get("difficulty")).toString(),
-                        testIngredientList,
-                        steps
-                    )
-                    Log.d("RECIPE_DB", "${recipe}")
-                    recipes.add(recipe)
-                    Log.d("RECIPE_DB1", "${recipes}")
+                val difficulty = document.getDouble("difficulty") ?: 0.0
+                if (countOneStar < 1 && difficulty <= 5) {
+                    filteredRecipes.add(document)
+                    countOneStar++
+                    Log.d("COUNT1", "${countOneStar}")
+                } else if (countTwoStar < 1 && difficulty in 5.1..10.0) {
+                    filteredRecipes.add(document)
+                    countTwoStar++
+                    Log.d("COUNT2", "${countTwoStar}")
+                } else if (countThreeStar < 1 && difficulty in 11.1..15.0) {
+                    filteredRecipes.add(document)
+                    countThreeStar++
+                    Log.d("COUNT3", "${countThreeStar}")
+                } else if (countFourStar < 1 && difficulty in 15.1..20.0) {
+                    filteredRecipes.add(document)
+                    countFourStar++
+                    Log.d("COUNT4", "${countFourStar}")
+                } else if (countFiveStar < 1 && difficulty > 20) {
+                    filteredRecipes.add(document)
+                    countFiveStar++
+                    Log.d("COUNT5", "${countFiveStar}")
                 }
+            }
+//            Log.d("COUNT", "${count}")
+            Log.d("RECIPE_DB_DIFFICULTY", "${filteredRecipes}")
+            for (document in filteredRecipes) {
+                val ingredients = document.data?.get("extendedIngredients") as List<Map<String, Any>>
+                //For ingredients mapping
+                for (ingredientData in ingredients) {
+                    var name = ingredientData["name"] as? String
+                    val quantity = ingredientData["amount"].toString()
+                    var unit = ingredientData["unit"] as? String
+                    val id = ingredientData["id"].toString()
+                    var og = ingredientData["original"] as? String
+                    var image = ingredientData["image"] as? String
+                    if(name == null){
+                        name =""
+                    }
+                    if(unit == null){
+                        unit =""
+                    }
+                    if(og == null){
+                        og =""
+                    }
+                    if(image == null){
+                        image =""
+                    }
+                    val ingredient = Ingredient(id, name, og, image, quantity, unit)
+                    testIngredientList.add(ingredient)
+                }
+                val steps = mutableListOf<Instruction>()
+
+                val instructions = document.data?.get("analyzedInstructions") as List<Map<String, Any>>
+                for(instructionData in instructions) {
+                    val stepNum = instructionData["number"] as Long
+                    val step = instructionData["step"].toString()
+
+                    val equipmentList = mutableListOf<Equipment>()
+                    //val equipment = instructionData.data?.get("equipment") as List<Map<String, Any>>
+                    val equipment = instructionData["equipment"] as List<Map<String, Any>>
+                    for(equipmentData in equipment) {
+                        val id = equipmentData["id"].toString()
+                        val name = equipmentData["name"].toString()
+                        val image = equipmentData["image"].toString()
+                        val e = Equipment(id, name, image)
+                        equipmentList.add(e)
+                    }
+
+                    val instructionIngredientsList = mutableListOf<Equipment>()
+                    val i = instructionData["ingredients"] as List<Map<String, Any>>
+                    for(instructionIngredientData in i) {
+                        val id = instructionIngredientData["id"].toString()
+                        val name = instructionIngredientData["name"].toString()
+                        val image = instructionIngredientData["image"].toString()
+                        val e = Equipment(id, name, image)
+                        instructionIngredientsList.add(e)
+                    }
+
+                    val instruction = Instruction(stepNum, step, instructionIngredientsList, equipmentList)
+                    steps.add(instruction)
+
+                }
+
+                var recipe = Recipe(
+                    (document.data?.get("id")).toString(),
+                    (document.data?.get("title")).toString(),
+                    (document.data?.get("image")).toString(),
+                    (document.data?.get("readyInMinutes")).toString() + " mins",
+                    (document.data?.get("servings")) as Long,
+                    (document.data?.get("cuisines")) as List<String>,
+                    (document.data?.get("vegetarian")) as Boolean,
+                    (document.data?.get("vegan")) as Boolean,
+                    (document.data?.get("glutenFree")) as Boolean,
+                    (document.data?.get("dairyFree")) as Boolean,
+                    (document.data?.get("veryHealthy")) as Boolean,
+                    (document.data?.get("cheap")) as Boolean,
+                    (document.data?.get("difficulty")).toString(),
+                    testIngredientList,
+                    steps
+                )
+                Log.d("RECIPE_DB", "${recipe}")
+                recipes.add(recipe)
+                Log.d("RECIPE_DB1", "${recipes}")
             }
             sharedViewModel.addCuisineRecipes(recipes)
         }
